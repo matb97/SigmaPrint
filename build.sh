9d489f80a20d4a37c11c48a0fd6632b272de6b05
matb97 <48256163+matb97@users.noreply.github.com>
2020-04-21 21:17:11 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,73 +3,32 @@ cd ~
 sudo apt update
 sudo apt upgrade -y
 
-#Removal of OctoPrint dependencies
-sudo apt remove -y python3-pip python3-dev python3-setuptools python3-virtualenv git libyaml-dev build-essential subversion libjpeg62-turbo-dev imagemagick ffmpeg libv4l-dev cmake
-
-#Removal of OctoPrint system files
-sudo rm -r OctoPrint
-sudo rm -r mjpg-streamer
-sudo rm -r .octoprint
-sudo rm -r scripts
-cd /etc/init.d
-sudo rm octoprint
-cd /etc/default
-sudo rm octoprint
-
-#Clean up
-sudo apt autoremove -y
-
-#Install OctoPrint dependencies
+#Update OctoPrint to 1.4.0
 cd ~
-sudo apt install -y python3 python3-pip python3-dev python3-setuptools python3-virtualenv git libyaml-dev build-essential subversion libjpeg62-turbo-dev imagemagick ffmpeg libv4l-dev cmake
-
-#Install OctoPrint system files
-mkdir OctoPrint && cd OctoPrint
-python3 -m virtualenv -p /usr/bin/python3 venv
-source venv/bin/activate
-sudo -H pip3 install --upgrade pip
-pip3 install octoprint==1.4.0
-sudo usermod -a -G tty pi
-sudo usermod -a -G dialout pi
-wget https://github.com/foosel/OctoPrint/raw/master/scripts/octoprint.init && sudo mv octoprint.init /etc/init.d/octoprint
-wget https://github.com/matb97/SigmaPrint/raw/master/octoprint.default && sudo mv octoprint.default /etc/default/octoprint
-sudo chmod +x /etc/init.d/octoprint
-sudo update-rc.d octoprint defaults
+source oprint/bin/activate
+pip install octoprint==1.4.0
 
 #Install Plugins
-pip3 install "https://github.com/OctoPrint/OctoPrint-DisplayProgress/archive/master.zip"
-pip3 install "https://github.com/jneilliii/OctoPrint-CustomBackground/archive/master.zip"
-pip3 install "https://github.com/derPicknicker1/OctoPrint-Mmu2filamentselect/archive/master.zip"
-pip3 install "https://github.com/eyal0/OctoPrint-PrintTimeGenius/archive/master.zip"
-pip3 install "https://github.com/jneilliii/OctoPrint-TPLinkSmartplug/archive/master.zip"
-pip3 install "https://github.com/marian42/octoprint-preheat/archive/master.zip"
-pip3 install "https://github.com/BrokenFire/OctoPrint-SimpleEmergencyStop/archive/master.zip"
-pip3 install "https://github.com/FormerLurker/Octolapse/archive/v0.3.4.zip"
+source venv/bin/activate
+sudo pip install --upgrade pip
+pip install "https://github.com/OctoPrint/OctoPrint-DisplayProgress/archive/master.zip"
+pip install "https://github.com/jneilliii/OctoPrint-CustomBackground/archive/master.zip"
+pip install "https://github.com/derPicknicker1/OctoPrint-Mmu2filamentselect/archive/master.zip"
+pip install "https://github.com/eyal0/OctoPrint-PrintTimeGenius/archive/master.zip"
+pip install "https://github.com/jneilliii/OctoPrint-TPLinkSmartplug/archive/master.zip"
+pip install "https://github.com/marian42/octoprint-preheat/archive/master.zip"
+pip install "https://github.com/BrokenFire/OctoPrint-SimpleEmergencyStop/archive/master.zip"
+pip install "https://github.com/FormerLurker/Octolapse/archive/v0.3.4.zip"
+pip install "https://github.com/TheSpaghettiDetective/OctoPrint-TheSpaghettiDetective/archive/master.zip"
 deactivate
 
-#Install webcam feed
-cd ~
-git clone https://github.com/jacksonliam/mjpg-streamer.git
-cd mjpg-streamer/mjpg-streamer-experimental
-export LD_LIBRARY_PATH=.
-make
-cd ~
-mkdir scripts
-wget https://github.com/matb97/SigmaPrint/raw/master/webcam && sudo mv webcam /home/pi/scripts/webcam
-wget https://github.com/matb97/SigmaPrint/raw/master/webcamDaemon && sudo mv webcamDaemon /home/pi/scripts/webcamDaemon
-chmod +x /home/pi/scripts/webcam
-chmod +x /home/pi/scripts/webcamDaemon
-cd /etc
-if grep -Fxq "/home/pi/scripts/webcam start " /etc/rc.local
-then
-    echo "rc.local already updated"
-else
-    sudo sed -i -e '$i /home/pi/scripts/webcam start \n' rc.local
-fi
-
 #Retrieve and apply Sigma settings
+#HAProxy
+cd /etc/haproxy
+sudo rm haproxy.cfg
+sudo wget https://github.com/matb97/SigmaPrint/raw/master/haproxy.cfg
+
 cd ~
-mkdir .octoprint
 cd .octoprint
 mkdir printerProfiles
 mkdir data
