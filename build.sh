baf3e5e45e79d1a881111a0897786ad5aeb7c57c
matb97 <48256163+matb97@users.noreply.github.com>
2020-06-06 13:48:52 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@
 cd ~
 sudo apt update
 sudo apt upgrade -y
-sudo apt-get install gawk util-linux qemu-user-static git p7zip-full python3
 
 #Build OctoPi
+sudo apt-get install gawk util-linux qemu-user-static git p7zip-full python3
 git clone https://github.com/guysoft/CustomPiOS.git
 git clone https://github.com/guysoft/OctoPi.git
 cd OctoPi/src/image
